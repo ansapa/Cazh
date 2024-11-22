5337c2fd68391145b727b43b01428240ab4082c7
Patrick Van den Bergh <patrick@ansapa.com>
2024-11-22 11:25:42 +0100

    Initial Commit

---
+++ Cazh/CazhApp.swift
@@ -0,0 +1,17 @@
+//
+//  CazhApp.swift
+//  Cazh
+//
+//  Created by Patrick Van den Bergh on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct CazhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CazhTests/CazhTests.swift
@@ -0,0 +1,16 @@
+//
+//  CazhTests.swift
+//  CazhTests
+//
+//  Created by Patrick Van den Bergh on 22/11/2024.
+//
+
+import Testing
+
+struct CazhTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
